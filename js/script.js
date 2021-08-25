6b5a1dced3d5b635cc46d2a9623ce56788a45a0f
Riki420 <81353585+Riki420@users.noreply.github.com>
2021-08-25 17:29:40 +0200

    Tabella studenti e media voti

---
+++ js/script.js
@@ -0,0 +1,27 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        students,
+    },
+    methods: {
+        getAverageGrades(valutation){
+            sum = 0;
+            valutation.forEach((voto) => {
+                sum += voto;
+            });
+
+            return (sum / valutation.length).toFixed(1);
+        },
+        getAverageGradesTot(vote){
+            let allVotes = [];
+            for(subject in vote) {
+                allVotes = allVotes.concat(vote[subject]);
+            }
+            return this.getAverageGrades(allVotes);
+        },
+        isFailed(voti){
+            return this.getAverageGradesTot(voti) < 6;
+
+        }
+    }
+})
